d5cca9ccc954a981d1555fb0e4a940689da95ce2
3Emme <emmettbuentiempo@gmail.com>
2020-09-04 13:52:26 -0700

    write solar_profile test 2.

--- __tests__/solar_profile.test.js
+++ __tests__/solar_profile.test.js
@@ -9,4 +9,8 @@ describe('SolarProfile', () => {
   test('Test 1: Should create a SolarProfile object, and populate properties with values given', () =>{    
     expect(profile1.earthAge).toEqual(28);
   });
+
+  test('Test 2: Should calculate earthRemain property values from earthAge and earthExpec property values', () =>{    
+    expect(profile1.earthRemain).toEqual(7);
+  });
 });
