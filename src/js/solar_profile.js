8c6363d66e33f43ce42da21c724bdc32f32da7dd
3Emme <emmettbuentiempo@gmail.com>
2020-09-04 16:52:43 -0700

    pass solar_profile test 15.

--- src/js/solar_profile.js
+++ src/js/solar_profile.js
@@ -14,6 +14,15 @@ export default class SolarProfile {
     this.marsRemain = this.marsExpec - this.marsAge,
     this.jupiterAge = Math.floor(earthAge * 11.86),
     this.jupiterExpec = Math.floor(earthExpec * 11.86),
-    this.jupiterRemain = this.jupiterExpec - this.jupiterAge
+    this.jupiterRemain = this.jupiterExpec - this.jupiterAge,
+    this.solarText = `The average life expectancy for your demographic on Earth is ${this.earthExpec}, and you're currently ${this.earthAge} years old. You've got ${this.earthRemain} more years to go!
+      
+    On Mercury the average life expectancy for your demographic is ${this.mercuryExpec}, and you're currently ${this.mercuryAge} years old (in Mercury years of course). You've got ${this.mercuryRemain} more Mercury years to go!
+
+    On Venus the average life expectancy for your demographic is ${this.venusExpec}, and you're currently ${this.venusAge} years old (in Venus years of course). You've got ${this.venusRemain} more Venus years to go!
+
+    On Mars the average life expectancy for your demographic is ${this.marsExpec}, and you're currently ${this.marsAge} years old (in Mars years of course). You've got ${this.marsRemain} more Mars years to go!
+    
+    On Jupiter the average life expectancy for your demographic is ${this.jupiterExpec}, and you're currently ${this.jupiterAge} years old (in Jupiter years of course). You've got ${this.jupiterRemain} more Jupiter years to go!`
   }
 }
