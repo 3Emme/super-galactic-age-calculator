0bde989f31a802c01109be391debb7aa9b3cf5df
3Emme <emmettbuentiempo@gmail.com>
2020-09-04 14:26:40 -0700

    pass solar_profile test 5.

--- src/js/solar_profile.js
+++ src/js/solar_profile.js
@@ -4,6 +4,7 @@ export default class SolarProfile {
     this.earthExpec = earthExpec,
     this.earthRemain = earthExpec - earthAge,
     this.mercuryAge = Math.floor(earthAge * .24),
-    this.mercuryExpec = Math.floor(earthExpec * .24)
+    this.mercuryExpec = Math.floor(earthExpec * .24),
+    this.mercuryRemain = this.mercuryExpec - this.mercuryAge
   }
 }
