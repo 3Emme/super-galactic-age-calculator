128b2c73196974e45ad2bd08dd85dceaacfa3bba
3Emme <emmettbuentiempo@gmail.com>
2020-09-04 12:43:09 -0700

    write solar_profile test 1.

---
+++ __tests__/solar_profile.test.js
@@ -0,0 +1,12 @@
+import SolarProfile from '../src/js/solar_profile.js';
+
+describe('SolarProfile', () => {
+  let profile1; 
+  beforeEach(() => {
+    profile1 = new SolarProfile(28,35)
+});
+
+  test('Test 1: Should create a SolarProfile object, and populate properties with values given', () =>{    
+    expect(profile1.earthAge).toEqual(28);
+  });
+});
